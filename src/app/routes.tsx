c36c5b8e66d1edef7eb4c53dc228c9c2312fa662
MiresPL <mateusz.skorek03@gmail.com>
2026-04-02 13:53:52 +0200

    poprawka 7

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router";
+import { createHashRouter} from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./pages/Home";
 import { AboutFoundation } from "./pages/AboutFoundation";
@@ -8,7 +8,7 @@ import { Achievements } from "./pages/Achievements";
 import { Partners } from "./pages/Partners";
 import { TrainingsClinic } from "./pages/TrainingsClinic";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         Component: Layout,
