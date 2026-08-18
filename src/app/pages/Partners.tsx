6eb1516f09775f2087cfd7728993a6a4170705e1
MiresPL <mateusz.skorek03@gmail.com>
2026-08-18 20:56:44 +0200

    dodanie programów i nowych partnerów

--- src/app/pages/Partners.tsx
+++ src/app/pages/Partners.tsx
@@ -42,6 +42,11 @@ export function Partners() {
 
   const strategicPartners: PartnerLink[] = [
     {
+      name: "Ministerstwo Sportu i Turystyki",
+      logo: "/pages/partners/msit.png",
+      url: "https://www.gov.pl/web/sport/ministerstwo-sportu-i-turystyki"
+    },
+    {
       name: "Polski Związek Jeździecki (PZJ)",
       logo: "/pages/partners/image20.png",
       url: "https://pzj.pl/"
@@ -50,6 +55,16 @@ export function Partners() {
       name: "Warszawsko-Mazowiecki Związek Jeździecki (WMZJ)",
       logo: "/pages/partners/image19.png",
       url: "https://wmzj.waw.pl/"
+    },
+    {
+      name: "Aktywni Lokalnie",
+      logo: "/pages/partners/aktywni_lokalnie.png",
+      url: "https://aktywni-lokalnie.pl/"
+    },
+    {
+      name: "Orły Sportu Fundacja",
+      logo: "/pages/partners/orly_sportu.png",
+      url: "https://orlysportu.pl/"
     }
   ];
 
@@ -98,19 +113,12 @@ export function Partners() {
       subprograms: [
         {
           id: "al-1",
-          name: "Cykl Spotkań",
+          name: "Cykl spotkań \"Piaseczno na koniu\" (2-5 dni)",
           items: [
             {
               id: "al-1-a",
-              title: "Spotkanie 1",
-              fullText: "Opis spotkania 1",
-              images: ["/pages/baner_Aktywni Lokalnie.png"],
-              instagramEmbedCode: ""
-            },
-            {
-              id: "al-1-b",
-              title: "Spotkanie 2",
-              fullText: "Opis spotkania 2",
+              title: "treningi w KJ Erren - Szumawa",
+              fullText: "",
               images: [],
               instagramEmbedCode: ""
             }
@@ -118,22 +126,113 @@ export function Partners() {
         },
         {
           id: "al-2",
-          name: "Poznajemy Stajenne Życie, od czyszczenia do treningu",
+          name: "Cykl spotkań \"Poznajemy życie stajenne-od czyszczenia do treningu\" (2-5 dni)",
           items: [
             {
-              id: "al-2-a",
-              title: "Podstawy opieki",
+              id: "al-1-a",
+              title: "12 sierpnia 2026 Caritas",
+              fullText: "W dniu 12 sierpnia 2026 r. w ramach programu Ministerstwa Sportu i\n" +
+                  "Turystyki - Aktywni Lokalnie Fundacja Grzegorza Kubiaka-Rozwój i Pasja\n" +
+                  "zaprosiła do stajni w Siedliskach małych i dużych mieszkańców\n" +
+                  "Piaseczna i okolic.\n\n" +
+                  "Gości powitała przedstawicielka Fundacji Magdalena Sapieszko-Kopania,\n" +
+                  "a następnie wolontariuszki i pracownicy stajni zaopiekowali się\n" +
+                  "gośćmi.\n\n" +
+                  "W ramach wizyty nasi goście zapoznali się z życiem stajennym, dzieci\n" +
+                  "pomagały przy czyszczeniu i siodłaniu koni. Następnie wszyscy\n" +
+                  "uczestniczyli w treningu pokazowym, gdzie na koniach zaprezentowały\n" +
+                  "się zawodniczki Fundacji Grzegorza Kubiaka-Rozwój i Pasja oraz sam\n" +
+                  "Trener Grzegorz Kubiak. Trening przyciągnął uwagę gości, ponieważ\n" +
+                  "konie skakały znakomicie, a treningi skokowe są zawsze bardzo\n" +
+                  "widowiskowe. Po treningu dzieci mogły przygotować posiłek dla koni, a\n" +
+                  "następnie przy pomocy pracowników Fundacji rozdawały konikom obiad.\n" +
+                  "Trener Grzegorz Kubiak jako uznany hodowca gołębi sportowych\n" +
+                  "zafundował dzieciom niespodziankę i zabrał chętnych do gołębnika,\n" +
+                  "gdzie dzieci mogły nakarmić gołębie.\n\n" +
+                  "Po pełnym atrakcji dniu wszyscy goście zostali zaproszeni na\n" +
+                  "przygotowany poczęstunek.  Na zakończenie dzieci mogły wybrać sobie\n" +
+                  "upominki z pudełka niespodzianek. Maskotki dla Uczestników ufundowała\n" +
+                  "Firma Chrupka.\n\n" +
+                  "Dziękujemy mieszkańcom za wizytę w naszej stajni. Mamy nadzieję, że\n" +
+                  "był to dzień pełen niezapomnianych wrażeń.",
+              images: [
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/1.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/2.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/3.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/4.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/5.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/6.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/7.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/8.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/9.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/10.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/11.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/12.jpg",
+                  "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/12-sierpnia-caritas/13.jpg",
+              ],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "al-1-b",
+              title: "13 sierpnia 2026 Caritas",
+              fullText: "Przygotowanie sprzętu i konia do treningu",
+              images: [
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/1.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/2.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/3.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/4.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/5.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/6.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/13-sierpnia-caritas/7.jpg",
+              ],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "al-1-c",
+              title: "17 sierpnia 2026 Caritas",
+              fullText: "17 sierpnia 2026 r na zaproszenie Fundacji Grzegorza Kubiaka-Rozwój i Pasja w ramach programu Ministerstwa Sportu i Turystyki Aktywni Lokalnie grupa dzieci z Piaseczna i okolic wraz z opiekunami miała okazję odwiedzić stajnię , gdzie czekało na nie wiele ciekawych atrakcji i niezapomnianych chwil. Była to doskonała okazja do bezpośredniego kontaktu z końmi, poznania ich zwyczajów oraz zdobycia nowych doświadczeń.\n" +
+                  "\n" +
+                  "Podczas pobytu dzieci zwiedziły stajnię i dowiedziały się, jak wygląda codzienna opieka nad końmi. Zobaczyły, czym konie są karmione, jak przygotowuje się je do jazdy oraz jak należy o nie dbać. Dużym zainteresowaniem cieszyło się czyszczenie koni – dzieci z zaangażowaniem szczotkowały ich sierść i uczyły się, jak bezpiecznie podchodzić do zwierząt.\n" +
+                  "\n" +
+                  "Najwięcej emocji wzbudził trening skokowy. Skoki przez przeszkody to zawsze bardzo widowiskowy i emocjonujący element pracy z koniem. Dla wielu dzieci było to pierwsze spotkanie z końmi z tak bliska, dlatego początkowo pojawiało się trochę niepewności. Szybko jednak zastąpiły ją uśmiechy, ciekawość i ogromna radość.\n" +
+                  "\n" +
+                  "Na wszystkich uczestników spotkania czekał poczęstunek oraz pudełko z niespodziankami. Maskotki ufundowała Firma Chrupka 😀 Dziękujemy !!!\n" +
+                  "\n" +
+                  "Wizyta w stajni była nie tylko świetną zabawą, ale również wartościową lekcją odpowiedzialności, cierpliwości i szacunku do zwierząt. Dzieci wróciły pełne pozytywnych emocji, nowych doświadczeń i pięknych wspomnień. Z pewnością chętnie odwiedzą stajnię ponownie!",
+              images: [
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/1.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/2.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/3.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/4.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/5.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/6.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/7.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/8.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/9.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/10.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/11.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/12.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/13.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/14.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/15.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/16.jpg",
+                "pages/partners/programs/aktywni-lokalnie/poznajemy-zycie-stajenne/17-sierpnia-caritas/17.jpg",
+              ],
+              instagramEmbedCode: ""
+            }
+          ]
+        },
+        {
+          id: "al-3",
+          name: "Zajęcia sportowe z treningiem ujeżdżeniowo-skokowym (10 bloków - 136 godzin)",
+          items: [
+            {
+              id: "al-1-a",
+              title: "12 sierpnia 2026 Caritas",
               fullText: "Nauka podstawowych czynności opiekuńczych nad koniem",
               images: [],
               instagramEmbedCode: ""
             },
-            {
-              id: "al-2-b",
-              title: "Przygotowanie do jazdy",
-              fullText: "Przygotowanie sprzętu i konia do treningu",
-              images: [],
-              instagramEmbedCode: ""
-            }
           ]
         }
       ]
@@ -149,24 +248,123 @@ export function Partners() {
       subprograms: [
         {
           id: "wm-1",
-          name: "Program Szkolenia",
+          name: "Rozmowy z Mistrzem",
           items: [
             {
               id: "wm-1-a",
-              title: "Podstawy skakania",
+              title: "Rozmowy z Mistrzem w Viart Kampinos",
               fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
               images: [],
               instagramEmbedCode: ""
             },
             {
               id: "wm-1-b",
-              title: "Zaawansowane techniki",
+              title: "Rozmowy z Mistrzem w Siedliskach na treningu",
               fullText: "Doskonalenie techniki jazdy na wyższym poziomie",
               images: [],
               instagramEmbedCode: ""
-            }
+            },
+            {
+              id: "wm-1-c",
+              title: "Rozmowy z Mistrzem z klasą kawalerii konnej",
+              fullText: "Doskonalenie techniki jazdy na wyższym poziomie",
+              images: [],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-1-d",
+              title: "Rozmowy z Mistrzem z Technikum Hodowli Koni",
+              fullText: "Doskonalenie techniki jazdy na wyższym poziomie",
+              images: [],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-1-e",
+              title: "Rozmowy z Mistrzem spotkanie na temat hejtu i social mediów z panią mecenas",
+              fullText: "Doskonalenie techniki jazdy na wyższym poziomie",
+              images: [],
+              instagramEmbedCode: ""
+            },
           ]
-        }
+        },
+        {
+          id: "wm-2",
+          name: "Treningi z Mistrzem (25 godzin)",
+          items: [
+            {
+              id: "wm-2-a",
+              title: "Treningi dzieci z Częstochowy",
+              fullText: "Fundacja Grzegorza Kubiaka Rozwój i Pasja w ramach programu " +
+                  "Ministerstwa Sportu i Turystyki Wielki Mistrz gościła w swojej stajni " +
+                  "zawodniczki z klubu Don Camillo w Konopiskach k/Częstochowy. " +
+                  "Dziewczynki uczestniczyły przez tydzień w treningu " +
+                  "ujeżdżeniowo-skokowym pod okiem wybitnego zawodnika i trenera " +
+                  "Grzegorza Kubiaka. Zawodniczki miały możliwość doskonalenia swoich " +
+                  "umiejętności, słuchania cennych uwag, a także obserwowania codziennej " +
+                  "pracy Wielkiego Mistrza z koniem i innymi zawodniczkami. Swoje " +
+                  "umiejętności jeździeckie zawodniczki będą miały szansę zaprezentować " +
+                  "podczas udziału w kolejnych zawodach w skokach przez przeszkody. " +
+                  "To był bardzo fajny czas spędzony na treningach i ciekawych rozmowach, " +
+                  "czas obfitujący w nowe doświadczenia i nowe umiejętności.",
+              images: [
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/1.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/2.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/3.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/4.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/5.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/6.jpg",
+                  "/pages/partners/programs/wielki-mistrz/trening-z-mistrzem/czestochowa/7.jpg",
+              ],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-2-b",
+              title: "Treningi Karolina Badowska",
+              fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
+              images: [],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-2-c",
+              title: "Treningi u Eweliny Polskiej-Fijałek",
+              fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
+              images: [],
+              instagramEmbedCode: ""
+            },
+          ]
+        },
+        {
+          id: "wm-3",
+          name: "Kliniki z Mistrzem",
+          items: [
+            {
+              id: "wm-3-a",
+              title: "Klinika szkoleniowa z Mistrzem Kozłów",
+              fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
+              images: [],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-3-b",
+              title: "Klinika szkoleniowa z Mistrzem Budki Żelazowskie",
+              fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
+              images: [],
+              instagramEmbedCode: ""
+            },
+            {
+              id: "wm-3-c",
+              title: "Klinika szkoleniowa z Mistrzem Częstochowa",
+              fullText: "Fundamentalne umiejętności w skakaniu przez przeszkody",
+              images: [
+                  "/pages/partners/programs/wielki-mistrz/kliniki-z-mistrzem/czestochowa/1.jpg",
+                  "/pages/partners/programs/wielki-mistrz/kliniki-z-mistrzem/czestochowa/3.jpg",
+                  "/pages/partners/programs/wielki-mistrz/kliniki-z-mistrzem/czestochowa/5.jpg",
+              ],
+              instagramEmbedCode: ""
+            },
+          ]
+        },
+
       ]
     }
   ];
